087e336b90586c9bc89793af26bb62023c6f9f81
NoKI-hub <nd.balykin@gmail.com>
2025-01-14 02:26:11 +0500

    ADD: new routers

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ from aiogram.client.default import DefaultBotProperties
 
 from config import config 
 from handlers.handlers import router
+from handlers.add_confs_handlers import add_conf_router
+from handlers.user_data_change_handlers import change_router
 
 
 async def main():
     bot = Bot(token=config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
+    dp.include_router(change_router)
+    dp.include_router(add_conf_router)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 

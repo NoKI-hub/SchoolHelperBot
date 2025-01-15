aedca7b24390d1dd2de3048b285bf2eddfdd2126
NoKI-hub <nd.balykin@gmail.com>
2025-01-16 02:50:21 +0500

    ADD: new admin command

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -6,10 +6,16 @@ from aiogram.fsm.context import FSMContext
 from filters import NotRegisteredFilter
 from states import UserStates
 from misc import user_data_validation, validation_handle
-from keyboards import base_keyboard
+from keyboards import user_keyboards
 
 
 router = Router()
+
+
+@router.message(Command('j'))
+async def jump(msg: Message, state: FSMContext):
+    from handlers.events_handlers import delete_event
+    await delete_event(msg, state)
 
 
 @router.message(Command('help'))
@@ -26,16 +32,16 @@ async def start_handler(msg: Message):
 async def start_not_registered_handler(msg : Message, state: FSMContext):
     await msg.answer("Здравствуйте! Я - бот, помогающий завучу в работе")
     await msg.answer("Для начала сбора данных напишите ниже своё ФИО")
-    await state.set_state(UserStates.names_processing)
+    await state.set_state(UserStates.NAMES_PROCESSING)
         
 
-@router.message(UserStates.names_processing)
+@router.message(UserStates.NAMES_PROCESSING)
 async def fullname_handler(msg: Message, state: FSMContext, is_admin: bool):
     result = user_data_validation(msg)
     if await validation_handle(result, msg):
         user = result
         ... # TODO добавление данных в базу
-        await msg.answer("Регистрация прошла успешно!", reply_markup=base_keyboard(is_admin))
-        await state.set_state(UserStates.base)
+        await msg.answer("Регистрация прошла успешно!", reply_markup=user_keyboards.base(is_admin))
+        await state.set_state(UserStates.BASE)
 
 

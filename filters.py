2cad83aeb57cb14a9562c85704fb8903462438fc
NoKI-hub <nd.balykin@gmail.com>
2025-01-15 22:56:26 +0500

    ADD: TextFilter and update StateFilter

--- filters.py
+++ filters.py
@@ -1,13 +1,18 @@
 from aiogram.filters import BaseFilter
-from aiogram.filters.state import State
+from aiogram.filters.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
+from misc import get_all_states
+
 
 class StatesFilter(BaseFilter):
-    def __init__(self, state: State | list[State]) -> None:
-        self.state = state
-        
+    def __init__(self, state: State | list[State] | StatesGroup) -> None:
+        if isinstance(state, StatesGroup):
+            self.state = get_all_states(state)
+        else:
+            self.state = state
+    
     async def __call__(self, message: Message, state: FSMContext) -> bool:
         if isinstance(self.state, State):
             return await state.get_state() == self.state
@@ -17,4 +22,13 @@ class StatesFilter(BaseFilter):
 class NotRegisteredFilter(BaseFilter):
     async def __call__(self, message: Message, state: FSMContext) -> bool:
         return await state.get_state() is None
+    
+
+class TextFilter(BaseFilter):
+    def __init__(self, texts: list[str]) -> None:
+        self.texts = texts
+
+    async def __call__(self, message: Message) -> bool:
+        return message.text in self.texts
+
     

d0f04b366e2ef5f631c7a4b036fc1357f9bb5a57
NoKI-hub <nd.balykin@gmail.com>
2025-01-15 23:09:27 +0500

    CREATE: AdminMiddleware

---
+++ middlewares/admin_middleware.py
@@ -0,0 +1,17 @@
+from aiogram import BaseMiddleware
+from aiogram.types import Message
+from typing import Dict, Any, Callable, Awaitable
+
+from config.config import ADMINS
+
+
+class AdminMiddleware(BaseMiddleware):
+    async def __call__(
+            self,
+            handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+            event: Message,
+            data: Dict[str, Any]
+    ) -> Any:
+        is_admin = event.from_user.id in ADMINS
+        data["is_admin"] = is_admin
+        await handler(event, data)

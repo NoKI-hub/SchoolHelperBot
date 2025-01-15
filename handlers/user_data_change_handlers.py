a73719d5488e3edaa1fe1a8ef51f71d1546b1cb4
NoKI-hub <nd.balykin@gmail.com>
2025-01-15 23:16:26 +0500

    ADD: admin verification CHANGE: handlers filters

--- handlers/user_data_change_handlers.py
+++ handlers/user_data_change_handlers.py
@@ -3,15 +3,15 @@ from aiogram.types import Message
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
-from user_states import UserStates
+from states import UserStates
 from misc import user_data_validation, validation_handle
-from keyboards import BASE_KEYBOARD, CHANGE_NAME_KEYBOARD
+from keyboards import base_keyboard, CHANGE_NAME_KEYBOARD
 
 
 change_router = Router()
 
 
-@change_router.message(Command('start'), UserStates.base)
+@change_router.message(F.text == "Изменить ФИО", UserStates.base)
 async def start_registered_handler(msg: Message, state: FSMContext):
     await msg.answer("Вы уже зарегистрированы. Желаете изменить свои данные?", reply_markup=CHANGE_NAME_KEYBOARD)
     await state.set_state(UserStates.change)
@@ -24,16 +24,16 @@ async def start_registered_handler(msg: Message, state: FSMContext):
 
 
 @change_router.message(UserStates.name_update)
-async def change_fullname_handler(msg: Message, state: FSMContext):
+async def change_fullname_handler(msg: Message, state: FSMContext, is_admin: bool):
     result = user_data_validation(msg)
     if await validation_handle(result, msg):
         user = result
         ... # TODO обновление данных в базе
-        await msg.answer("Изменение прошло успешно!", reply_markup=BASE_KEYBOARD)
+        await msg.answer("Изменение прошло успешно!", reply_markup=base_keyboard(is_admin))
         await state.set_state(UserStates.base)
         
 
 @change_router.message(F.text == "Отмена", UserStates.change)
-async def cancel_change_fullname(msg: Message, state: FSMContext):
-    await msg.answer("Изменение отменено", reply_markup=BASE_KEYBOARD)
+async def cancel_change_fullname(msg: Message, state: FSMContext, is_admin: bool):
+    await msg.answer("Изменение отменено", reply_markup=base_keyboard(is_admin))
     await state.set_state(UserStates.base)

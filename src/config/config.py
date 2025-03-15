3114b94e056cec2e957ea906dfcbbb582f5345c9
space_verse <f-balykin@mail.ru>
2025-03-16 01:08:45 +0500

    MAKE: src/ folder; MAKE: Dockerfile && docker-compose.yaml && .dockerignore; MODIFY: .gitignore;

--- src/config/config.py
+++ src/config/config.py
@@ -11,14 +11,14 @@ class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
-    DB_PASS: str
+    DB_PASSWORD: str
     DB_NAME: str
     BOT_TOKEN: str
     ADMINS: str | list[int]
 
     @property
     def DB_URL(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
     @property
     def DEBUG_DB_URL(self):
